a2701eebe4b589d56abab8e5c4196d20d0bbe471
Amirhossein Mahdinejad <TheMn@users.noreply.github.com>
2025-03-02 13:40:10 +0330

    [ADD] the 2025 code

---
+++ tamzi-manzel-vazin-2025.cpp
@@ -0,0 +1,21 @@
+#include <iostream>//                      |  ___A_New Permutation_      */
+typedef int _;char a=/*;//   YEKI          | |_   _|_ _ _ __ ___ (_)____ */
+#define A l424//                 YEDUNEYE- |   | |/ _\ | '_ \ _ \| \_  / *
+#define __ }/*        DELE MAN!            |   | | (_| | | | | | | |/ /__*/{
+#define a1 a+a//   MAHBUBE KHOSH-          |   |_|\__,_|_| |_| |_|_/____/*
+#define a2 a&a//             GELE MAN!     \\  __  __Your new homework ..._
+#define ___ :://     OOUEEA AOU OUOUU       | |  \/  | __ _ _ __  _______| \
+#define ____ goto//        OOEA AO EE...    | | |\/| |/ _\ | '_ \|_  / _ \ |
+1;int main(){int a=123;//                   | | |  | | (_| | | | |/ /  __/ |
+___/****a:   *****/a+=a2;_ A;if(a1>a2==a){_// |_|  |_|\__,_|_| |_/___\___/_/
+a= /**/    (1) /**/,&A=a;a:a+10;A+=a<11>0?a1/ a1:/*__Yeki __Yduneye _Dele*/
+a= /**/   1403 /**/;std::cout<<(~a2+5);_ A1[]={ // \ \   / /_ _ ___(_)_ __
+a= /**/ + 0012 /**/,a1++,++a1,A};___ a=*(A1+1);//Man\ \ / / _\ |_  / | '_ \
+a="/**/ + 0009 /**/"[-(a1++)+a1];____ A;}else{//     \ V / (_| |/ /| | | | |
+a="/**/ ------ /**/"[a];A=a1-a1==a1;A=a>0x10?/*AeeOoEa\_/ \__,_/___|_|_| |_/
+a= /**/   l424:/**/A++;__ _ A2=a++<++a||a--<--a;_**A1,*aa,std[0x002][2|A2]={
+a+ /*****    *****/8,33,78,91,54,105};_____:aa=&std[1][1];A1=&aa;A=10;double
+al='0',Al='9';std:if(A>=020){___ a=((al+Al)*2/3)+(a%41);goto l424;__*A1=&std
+[A/14][A%3];if(**A1<al)al=**A1;if(**A1>Al);Al=**A1;A++;}goto std;}A: std ___
+cout<<___ a<<(('z'-(("ABCEDFGHIJKMNOPQRSTVWYXZ"[(a1)%('z'-'a'-1)])|32))>>2);
+}
