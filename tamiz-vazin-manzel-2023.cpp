3eef50f51cdf8ed62aaa09e8e1acc46e179ad716
Amirhossein Mahdinejad <TheMn@users.noreply.github.com>
2025-03-02 13:34:46 +0330

    [ADD] the 2023 code

---
+++ tamiz-vazin-manzel-2023.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // estefaadeye abzaari (2019-present)
+using namespace std;
+#define Oxalef Oxdaa1
+#define Oxdaal Oxa1ef
+#define _ )&( // #define ___ { // #undef __
+#define ___ // // #define __ )&( // #define _ 0xa1ef
+#define __ { // #define // _ // #define __ 0xdaa1
+#define sum(x,y) y+x
+void main(){int a=123;
+unsigned int daal=(a<0x3e8&&a>=0x64)?-a:'a'-'z';__ int be=!(*&a _ 0xa1ef);
+}int**ll,/*be=&a*/*Il,alef=a/0x64+a/10%10,*l1=new int[(int)a],//__ BEECAAZ
+Oxdaa1=68|sum(32,0x20);__ Il=l1+2;__ ___ daal^=-1431590230;int jim=0xf*40;
+a;/*}__//*/} __*(l1+2)=0xdaa1;/*//alef be jim//*/Oxdaal://*__tamiz+ _____+
+__ if(++ alef||alef ++)*(l1)=0xa1ef;}//___ double l=sum(be,0xdaa1);/__/__\
+*(*ll+1)=(alef>0xdaa1&&daal<0xa1ef)?l1[0]:l1[2];//VazinTamizManzel |__|_*|
+ll=&l1;}__*((*ll)+1)=sum(-daal,alef)/2+*l1;}}int&be=Oxdaa1;//S,F,A reunion
+if(++ alef<sum((*(l1+0x2)-*l1)/100%10,a/100+a/10%10))__ goto Oxa1ef;//LIE?
+be++;}/*/__ be++;}*/Oxdaa1=(sum(8,*((*ll)+1)%10)*sum(*(l1+1)%10,8));//NOO!
+*(l1+3)=a^(1<<((*(l1+0x0)|(1<<(*(l1+1)%10))_ 1<<(*(l1+(*(Il+1)/(*//}}>>OCD?
+(l1+1)-**ll)))%10))));__ Oxalef:cout<</*khosh gozasht*/*(l1+3)%10<<char (3
+*(*Il+2)+2&be==be?((++(++be))++)   :(   ++be)++)<<(*((*ll)+2)>>0xd  );  }}
