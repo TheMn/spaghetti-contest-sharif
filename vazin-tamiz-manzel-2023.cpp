edffa2ddc8e799a1a2226790645c148b4c07f179
Amirhossein Mahdinejad <TheMn@users.noreply.github.com>
2025-03-02 14:51:35 +0330

    [FIX] change void to int

--- vazin-tamiz-manzel-2023.cpp
+++ vazin-tamiz-manzel-2023.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define ___ // // #define __ )&( // #define _ 0xa1ef
 #define __ { // #define // _ // #define __ 0xdaa1
 #define sum(x,y) y+x
-void main(){int a=123;
+int main(){int a=123;
 unsigned int daal=(a<0x3e8&&a>=0x64)?-a:'a'-'z';__ int be=!(*&a _ 0xa1ef);
 }int**ll,/*be=&a*/*Il,alef=a/0x64+a/10%10,*l1=new int[(int)a],//__ BEECAAZ
 Oxdaa1=68|sum(32,0x20);__ Il=l1+2;__ ___ daal^=-1431590230;int jim=0xf*40;
